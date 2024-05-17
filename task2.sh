9d027aad7a7d99b5ef3304a8d81946acaab6aaee
David <deeseinmedia@gmail.com>
2024-05-17 10:24:54 +0200

    updated the skript

---
+++ task2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Task2 a little more advanced 2nd Script
+
+echo "Hello, World!"
+echo "Today is: `date`"
+echo "You are logged in as: `whoami`"
+echo "Your current working directory is: `pwd`"
