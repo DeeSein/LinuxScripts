f260c4a6c25feebce189073df505686f46b1e519
David <deeseinmedia@gmail.com>
2024-05-17 10:48:01 +0200

    fixed the > < to -lt and -gt

--- task4.sh
+++ task4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # My 4th script for basic number comparison
 
-if [ "$1" > "$2" ]
+if [ "$1" -gt "$2" ]
 then
 	echo "The first number is greater than the second number."
-elif [ "$1" < "$2" ]
+elif [ "$1" -lt "$2" ]
 then
 	echo "The second number is greater than the first number."
 else
