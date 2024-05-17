fa9bbfaee24654a8f72db36b1f2a6f207dc454cf
David <deeseinmedia@gmail.com>
2024-05-17 16:11:04 +0200

    Added right code to the script

--- task5.sh
+++ task5.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# Creating new textfiles by numbers and name
 
-for i in 1 2 3 4 5 6 7 8 9 
-do
-	echo "" $i > file$i.txt
+echo "How many .txt files do you want to create?"
+read count
+
+echo "Enter the directory path to save the files (leave blank for current directory):"
+read dir_path
+
+if [ -z "$dir_path" ]; then
+    dir_path="."
+fi
+
+counter=1
+while [ $counter -le $count ]; do
+    file_name="file$counter.txt"
+    touch "$dir_path/$file_name"
+    echo "Created $file_name in $dir_path"
+    counter=$((counter+1))
 done
+
+echo "Done creating $count.txt files in $dir_path"
+
