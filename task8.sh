b3322c1dfcdf99422ee476fef5e42f6ed1ad6ed8
David <deeseinmedia@gmail.com>
2024-05-20 14:28:01 +0200

    fixed the mkdir and added descriptions

--- task8.sh
+++ task8.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-#Author: David Becker
-#Date: 2024-05-20
+# Author: David Becker
+# Date: 2024-05-20
 
 # Task8 script for backup of a directory
 
-echo "Insert the absolut path of the directory you want to backup:"
-read BACKUP
-echo "Insert the absolut path of the destination you want the backup to store:"
-read DESTINATION
+read -p "Enter the absolute path of the directory you want to backup: " OUTPUT
+read -p "Enter the absolute path of the destination for the backup: " DESTINATION
+read -p "Enter a name for the backup file: " FILENAME
 
-tar -cvf backup-home.tar $BACKUP /$DESTINATION
-echo “backup job complete at $DESTINATION, `date`
+# Create the destination directory if it doesn't exist
+mkdir -p "$DESTINATION"
+
+# Create the tar archive
+tar -cvf "$DESTINATION/$FILENAME.tar" "$OUTPUT"
+
+echo "Backup job complete at $DESTINATION, $(date)"
+
